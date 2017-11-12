a55e2699823464d7d4da2a799cda862f57286e30
lucas lange <lucas@gtx.ag>
2017-11-12 19:23:21 -0200

    creates grid that changes color and size by user input

---
+++ js/app.js
@@ -0,0 +1,26 @@
+
+
+$(function makeGrid(){
+  //Select the Submit button
+  $('.inputBtn').on('click', function(e){
+    e.preventDefault();
+
+    const color = $('#colorPicker').val()
+    //selects the number of rows and columns by input
+    const rows = $('#input_height').val();
+    const columns = $('#input_width').val();
+    console.log(rows,columns);
+
+    //the first loop serves for creating the rows of the grid
+    for (let x = 1; x<=rows; ++x){
+      $('#pixel_canvas').append('<tr class="rowStructure"></tr>');
+
+
+      //this second loop to create the column in each row. That is why is used the pseudo-class 'last-of-type', other wise it would keep adding to all previous rows.
+      for (let y = 1; y<=columns; ++y){
+        $('.rowStructure:last-of-type').append('<td></td>');
+        $('td').css('background-color',color);
+      }
+    }
+  })
+});
