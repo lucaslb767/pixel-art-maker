0c4f7cbeba71cce936904550e6ebd1dec2318c8a
lucas lange <lucas@gtx.ag>
2017-11-13 04:06:33 -0200

    fix: Removes previous grid when submit button is clicked

--- js/app.js
+++ js/app.js
@@ -12,12 +12,15 @@ $(function makeGrid(){
     const rows = $('#input_height').val();
     const columns = $('#input_width').val();
 
+    //remove the old grid when user clicks in the submit button
+    $('.rowStructure').remove();
+
     //the first loop serves for creating the rows of the grid
     for (let x = 1; x<=rows; ++x){
       $('#pixel_canvas').append('<tr class="rowStructure"></tr>');
 
 
-      //this second loop to create the column in each row. That is why is used the pseudo-class 'last-of-type', other wise it would keep adding to all previous rows.
+      //this second loop serves to create the column in each row. That is why is used the pseudo-class 'last-of-type', otherwise it would keep adding to all previous rows.
       for (let y = 1; y<=columns; ++y){
         $('.rowStructure:last-of-type').append('<td></td>');
         $('td').css('background-color',color);
